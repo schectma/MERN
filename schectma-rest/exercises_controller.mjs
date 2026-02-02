7fe95897fb65e4825b3bddc5010e4effbe6cb333
schectma <schectma@oregonstate.edu>
2026-02-02 17:35:29 -0500

    added cors dep

--- schectma-rest/exercises_controller.mjs
+++ schectma-rest/exercises_controller.mjs
@@ -2,12 +2,21 @@ import 'dotenv/config';
 import * as exercises from './exercises_model.mjs';
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import cors from 'cors';
 
 const PORT = process.env.PORT;
 
 const ERROR_NOT_FOUND = {Error: 'Not found'}
 
 const app = express();
+
+app.use(cors({
+    origin: [
+        'https://dazzling-freedom-production.up.railway.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 app.use(express.json());
 
