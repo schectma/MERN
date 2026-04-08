6f02a8b18c68a3ec20625919fefb5045b579e352
schectma <schectma@oregonstate.edu>
2026-04-08 17:31:18 -0400

    placeholder text update

--- schectma-rest/exercises_controller.mjs
+++ schectma-rest/exercises_controller.mjs
@@ -30,8 +30,7 @@ app.listen(PORT, async ()=>{
 * Return true if the date format is MM-DD-YY where MM, DD and YY are 2 digit integers
 */
 function isDateValid(date) {
-    // Test using a regular expression. 
-    // To learn about regular expressions see Chapter 6 of the text book
+    // Test using a regular expression.
     const format = /^\d\d-\d\d-\d\d$/;
     return format.test(date);
 }
