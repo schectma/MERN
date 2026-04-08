40fb3cfc02d4806ae210187a4a3eb4e20f2de3ba
schectma <schectma@oregonstate.edu>
2026-04-08 18:10:55 -0400

    homepage mod

--- schectma-react/src/components/ExerciseCollection.jsx
+++ schectma-react/src/components/ExerciseCollection.jsx
@@ -1,6 +1,10 @@
 import ExerciseItem from './ExerciseItem';
 
 function ExerciseCollection({ exercises, onDelete, onEdit}) {
+    if (!exercises.length) {
+        return <p className="home-empty">No exercises yet. Add one to get started.</p>;
+    }
+
     return (
         <div className="collection-container">
             {exercises.map((exercise, i) => <ExerciseItem exercise={exercise} 
